7a1572e61e80fa69bdbcf6deca114d24e3a7b932
Sarbeng <sarbengbaafi@gmail.com>
2023-08-10 13:46:36 +0000

    model and controller added for blog

--- portfolio_api/database/migrations/2023_08_10_125253_create_blogs_table.php
+++ portfolio_api/database/migrations/2023_08_10_125253_create_blogs_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100)->nullable()->default('text');
+            $table->string('title', 150)->nullable()->default('title');
             $table->longText('description')->nullable()->default('text');
+            $table->string('image', 150)->nullable()->default('image');
             $table->timestamps();
         });
     }
