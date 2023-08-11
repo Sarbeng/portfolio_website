e92e694487ddb84ed4780eb2c06f1d6c247dd54d
Sarbeng <sarbengbaafi@gmail.com>
2023-08-11 22:45:35 +0000

    open sesame

--- portfolio_api/app/Http/Controllers/Api/BlogController.php
+++ portfolio_api/app/Http/Controllers/Api/BlogController.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\Controller;
 use App\Models\Blog;
 use Illuminate\Http\Request;
 use App\Http\Requests\StoreBlogRequest;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\File;
+use Illuminate\Support\Facades\URL;
 
 class BlogController extends Controller
 {
@@ -36,8 +40,37 @@ class BlogController extends Controller
      */
     public function store(Request $request)
     {
-        //
-        $blog = Blog::create($request->all());
+        //data validation
+        $data = $request->all();
+
+        $validator = Validator::make($data,[
+            'title' => 'required|max:150',
+            'description' => 'required',
+            'image' => [File::image()->max(2 * 1024)]
+
+        ]);
+
+        //$file = $request->file('image');
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $image = $request->file('image');
+        $imageName = $image->hashName();
+        $extension = $image->extension();
+        $filename = uniqid() . '_' . $imageName;
+        $image->move(public_path('public/images'),$filename);
+        //Storage::disk('public')->putFileAs('images', $image, $filename);
+        $url = URL::to('/') . '/public/images' . $filename;
+
+        $formData = [
+            'title' =>  $data['title'],
+            'description' => $data['description'],
+            'image' => $url
+        ];
+
+        $blog = Blog::create($formData);
 
         return response()->json([
             'status' => true,
